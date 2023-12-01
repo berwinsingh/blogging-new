c3e02e361d3c53f8483001e031d54e2505603df1
Berwin Singh <63018717+berwinsingh@users.noreply.github.com>
2023-12-01 14:14:49 +0530

    Added Mobile responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/header'
 import HeroSection from '@/components/hero-section'
 import { titillium_web } from '@/components/font'
+import BlogPreviewCard from '@/components/blog-preview'
 
 export default function Home() {
   return (
@@ -8,8 +9,8 @@ export default function Home() {
         <Header />
         <HeroSection/>
         <h2 className={`${titillium_web.className} font-bold text-2xl p-4`}>Recent blog posts</h2>
-        <div id="blog-posts" className="p-4 mt-2">
-            
+        <div id="blog-posts" className="p-4 mt-2 flex flex-wrap">
+            <BlogPreviewCard />
         </div>
     </main>
   )
--- components/blog-preview.tsx
+++ components/blog-preview.tsx
@@ -1,7 +1,18 @@
 import { titillium_web } from "./font";
 import Image from "next/image";
 
-const BlogPreviewCard = ()=>{
+
+type BlogDetails = {
+    title: string,
+    description: string,
+    image: string,
+    date: string,
+}
+
+const date = new Date();
+
+
+const BlogPreviewCard = ({title, description,image,date}:BlogDetails)=>{
     return(
         <>
         
